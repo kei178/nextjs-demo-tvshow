e22652497d2fa8dc535cee98e78ff5f8889bf309
Kei178 <keisuke.inaba.cpa@gmail.com>
2020-06-09 22:02:52 +1000

    Add original fonts

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,15 @@ function MyApp({ Component, pageProps }) {
       <Header />
       <Component {...pageProps} />
       <style jsx>{`
+        @font-face {
+          font-family: 'raleway';
+          src: url('/fonts/raleway/Raleway-Regular.ttf') format('truetype');
+        }
+
+        :global(html) {
+          font-family: 'raleway';
+        }
+
         :global(ul) {
           padding: 0;
           margin: 0;
